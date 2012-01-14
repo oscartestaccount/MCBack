1c0ca08618fd8df5aa99e335c6f08bd0c7c0a638
oscartestaccount <freedive75@hotmail.com>
2012-01-14 19:18:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MCBack::Application.config.secret_token = 'b5a624fdfd81645c15797bb98b85e0085c3f8a25c43566c82413ef9220f0b44b160882042aa58f0b1e16ffc0d6bafe58d0889afc49d488fbcc571029d49a39e1'
